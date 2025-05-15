827ff7618d07e45b637876059a05b3eaaef4dbcb
plettm <148540702+plettm@users.noreply.github.com>
2025-05-15 11:08:12 +0200

    Update firebase.py

--- firebase.py
+++ firebase.py
@@ -1,6 +1,11 @@
-import firebase_admin
-from firebase_admin import credentials, firestore
+import os
+import json
+from firebase_admin import credentials, firestore, initialize_app
 
-cred = credentials.Certificate("firebase_key.json")
-firebase_admin.initialize_app(cred)
+# Firebase-Key aus Umgebungsvariable lesen
+firebase_key = json.loads(os.environ["FIREBASE_KEY"])
+cred = credentials.Certificate(firebase_key)
+
+# Initialisieren
+initialize_app(cred)
 db = firestore.client()
